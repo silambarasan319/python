179ad0059a0d11868c80b51ce1477c9616414844
silambarasan319 <61216729+silambarasan319@users.noreply.github.com>
2020-06-12 19:24:26 +0530

    rewrite

--- windows_filesystem.py
+++ windows_filesystem.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.8
 import wmi
 import sys
+import os
 import requests
 
 c = wmi.WMI ()
@@ -8,29 +9,23 @@ var=(int(format(c.Win32_LogicalDisk()[0].Freespace))/int(format(c.Win32_LogicalD
 var=int(var)
 
 if var<=50:
-   print ("1 - Got a true expression value")
-   print (var)
-   exit(0)
+   print ( "OK - - C:\\ Drive Used Space",var,"%")
+   sys.exit(0)
 elif 60<=var & var<=79:
-   print ("2 - Got a true expression value")
+   print ( "Warning - C:\\ Drive Used Space",var,"%"  )
    headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'X-Rundeck-Auth-Token': '8h7eFvQ1Nb5JrhulUdYrHvAKzwWw18gp',
+        'X-Rundeck-Auth-Token': 'GLZA6ZIBJENbPPvePyrRqpOhW8g7gAE9',
    }
    data = '{"argString":"-servername servername -filesystem redhat "}'
-   response = requests.post('http://192.168.1.10:4440/api/16/job/2ed8d8c9-3f46-46d3-ba5f-e71d3211a40c/executions', headers=headers, data=data)
-   print (var)
-   exit(1)
-elif 80<=var & var<=95:
-   print ("3 - Got a true expression value")
-   print (var)
-   exit(2)
+   response = requests.post('http://192.168.1.10:4440/api/16/job/f26a56bf-1974-4f7a-b8f4-37162e52188d/executions', headers=headers, data=data)
+   sys.exit(1)
+elif 80<=var & var<=99:
+   print ( "CRITICAL  - C:\\ Drive Used Space",var,"%")
+   sys.exit(2)
 else:
-   print ("4 - Got a false expression value")
-   print (var)
-   exit(3)
+   print ( "UKNOWN  - C:\\ Drive Used Space",var,"%")
+   sys.exit(3)
 
 print ("Good bye!")
-
-
