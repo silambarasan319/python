ffede4d8588c45b384162a548ac031d259b1e135
silambarasan319 <61216729+silambarasan319@users.noreply.github.com>
2020-06-13 01:41:51 +0530

    main

--- windows_filesystem.py
+++ windows_filesystem.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3.8
-import wmi
-import sys
-import os
-import requests
+import wmi, sys, os, requests
 
-c = wmi.WMI ()
-var=(int(format(c.Win32_LogicalDisk()[0].Freespace))/int(format(c.Win32_LogicalDisk()[0].Size)))*100
-var=int(var)
+def functionA(var):
+    if var<=50:
+        print ( "OK - - C:\\ Drive Used Space",var,"%")
+        sys.exit(0)
+    elif 60<=var & var<=79:
+        print ( "Warning - C:\\ Drive Used Space",var,"%"  )
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-Rundeck-Auth-Token': 'GLZA6ZIBJENbPPvePyrRqpOhW8g7gAE9',
+        }
+        data = '{"argString":"-servername servername -filesystem redhat "}'
+        response = requests.post('http://192.168.1.10:4440/api/16/job/f26a56bf-1974-4f7a-b8f4-37162e52188d/executions', headers=headers, data=data)
+        sys.exit(1)
+    elif 80<=var & var<=99:
+        print ( "CRITICAL  - C:\\ Drive Used Space",var,"%")
+        sys.exit(2)
+    else:
+        print ( "UKNOWN  - C:\\ Drive Used Space",var,"%")
+        sys.exit(3)
 
-if var<=50:
-   print ( "OK - - C:\\ Drive Used Space",var,"%")
-   sys.exit(0)
-elif 60<=var & var<=79:
-   print ( "Warning - C:\\ Drive Used Space",var,"%"  )
-   headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-Rundeck-Auth-Token': 'GLZA6ZIBJENbPPvePyrRqpOhW8g7gAE9',
-   }
-   data = '{"argString":"-servername servername -filesystem redhat "}'
-   response = requests.post('http://192.168.1.10:4440/api/16/job/f26a56bf-1974-4f7a-b8f4-37162e52188d/executions', headers=headers, data=data)
-   sys.exit(1)
-elif 80<=var & var<=99:
-   print ( "CRITICAL  - C:\\ Drive Used Space",var,"%")
-   sys.exit(2)
-else:
-   print ( "UKNOWN  - C:\\ Drive Used Space",var,"%")
-   sys.exit(3)
+    print ("Good bye!")
 
-print ("Good bye!")
+if __name__ == '__main__':
+    c = wmi.WMI ()
+    var=(int(format(c.Win32_LogicalDisk()[0].Freespace))/int(format(c.Win32_LogicalDisk()[0].Size)))*100
+    var=int(var)
+    functionA(var)
