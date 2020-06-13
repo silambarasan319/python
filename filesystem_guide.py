611abc27c479d369f4262d8a56ac071d32aac360
silambarasan319 <61216729+silambarasan319@users.noreply.github.com>
2020-06-13 20:17:40 +0530

    two

--- filesystem_guide.py
+++ filesystem_guide.py
@@ -4,4 +4,29 @@ if __name__ == "__main__":
         print root
         print dirs
         print files
-        print '--------------------------------'
+-------------------------------------------------------------------------------------------
+import os
+def main():
+    # Create directory
+    dirName = 'D:\\destination\\google'
+    # Create target directory & all intermediate directories if don't exists
+    try:
+        os.makedirs(dirName)    
+        print("Directory " , dirName ,  " Created ")
+    except FileExistsError:
+        print("Directory " , dirName ,  " already exists")  
+             
+if __name__ == '__main__':
+    main()
+    path = os.getcwd()
+    print ("The current working directory is %s" % path)
+-------------------------------------------------------------------------------------------------
+def main():
+
+    # Get the modification time
+    t = time.ctime(path.getmtime("disk.py"))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime("disk.py")))
+
+if __name__ == "__main__":
+    main()
